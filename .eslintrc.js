c7d1f2dd2fdc8c1ac1caea934203dbad5e076274
Nazmi Altun <nazmialtun@windowslive.com>
2021-03-29 01:00:01 +0800

    fix  linting configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,16 @@
 module.exports = {
+    "root": true,
     extends: [
-        "eslint:recommended",
-        "plugin:react/recommended",
+        'eslint:recommended',
+        'plugin:react/recommended',
         'prettier',
-        "plugin:jest/recommended",
-        "plugin:testing-library/react",
-        "plugin:jest-dom/recommended"
+        'plugin:jest/recommended',
+        'plugin:testing-library/react',
+        'plugin:testing-library/recommended',
+        'plugin:jest-dom/recommended'
     ],
-    plugins: ['react', 'jest',"testing-library", "jest-dom"],
+    plugins: ['prettier', 'react', 'jest', 'testing-library', 'jest-dom'],
     "rules": {
-        "testing-library/no-render-in-setup": "error",
-        "testing-library/no-wait-for-empty-callback": "error",
-        "testing-library/prefer-explicit-assert": "error",
-        "testing-library/prefer-presence-queries": "error",
-        "testing-library/prefer-screen-queries": "error",
-        "testing-library/prefer-wait-for": "error"
-      }
+        "prettier/prettier": ["error"]
+    },
 };
