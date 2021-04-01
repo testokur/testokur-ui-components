25563d617f3f3938069569c1dbf33ecd46f54618
Nazmi Altun <nazmialtun@windowslive.com>
2021-04-01 23:28:23 +0800

    use .json extension for configs

--- .stylelintrc.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
-  rules: { 'value-keyword-case': ['lower', { ignoreKeywords: ['BlinkMacSystemFont'] }] },
-};
